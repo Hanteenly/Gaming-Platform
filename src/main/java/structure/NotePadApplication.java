cd85de5ae06208377b3578c60694ff8145882a38
Maksim <maksym.mezhakov@student.tuke.sk>
2026-04-09 22:40:37 +0200

    up

---
+++ src/main/java/structure/NotePadApplication.java
@@ -0,0 +1,10 @@
+package structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotePadApplication {
+
+
+}
